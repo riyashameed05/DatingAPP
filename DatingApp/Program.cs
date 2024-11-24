f490b93f0e3eea05d14099931a9c5e439e928837
riyashameed05 <riyashameed50@gamil.com>
2024-11-24 18:40:15 +0530

    End of Angular Skeleton

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -10,7 +10,11 @@ builder.Services.AddDbContext<DataContext>(option =>
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(x => x.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyMethod().AllowAnyHeader());
 
 app.MapControllers();
 
